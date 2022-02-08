495aba1fd8a9673d40841e37213e462893a9d9f9
Wossyy <acra2024@aol.com>
2022-02-08 13:02:21 -0600

    new finished product

--- Assignments/functions_practice.py
+++ Assignments/functions_practice.py
@@ -68,12 +68,72 @@ max(input("Please enter your first number: "),
 # list_sum()
 
 
+# 6. List sum
 def list_sum(list):
     sum = 0
     for nums in list:
         sum = nums + sum
-        return sum
+    return sum
 
 
 list = [1, 2, 3]
 print(list_sum(list))
+
+
+# 7. In range
+def in_range(max):
+    if max >= num:
+        return True
+    else:
+        return False
+
+
+max = int(input("Please enter a max number: "))
+num = int(input("Please enter your number: "))
+print(in_range(max))
+
+
+# 8. Find evens
+def find_evens(nums):
+    evens = []
+    for num in nums:
+        if num % 2 == 0:
+            evens.append(num)
+    return evens
+
+
+print(find_evens([1, 2, 3, 4]))
+
+
+# 9. Greeting
+def greet_me(name):
+    if greeting == "y":
+        greetings = ["What's up ", "Howdy ", "How are you? "]
+        final = random.choice(greetings)
+        return final + name
+    elif greeting == "n":
+        return "Hello " + name
+
+
+name = input("Please enter your name: ")
+greeting = input("Do you want a special greeting? Please enter y/n: ")
+print(greet_me(name))
+
+
+# 10. simple calculator
+def calculator(num1, num2, operation):
+    if operation == "+":
+        return num1 + num2
+    elif operation == "-":
+        return num1 - num2
+    elif operation == "/":
+        return num1 / num2
+    elif operation == "*":
+        return num1 * num2
+
+
+print("Welcome to this calculator program!\nEnter + to add\nEnter - to subtract\nEnter / to divide\nEnter * to multiply")
+num1 = int(input("Please enter a number: "))
+num2 = int(input("Please enter a second number: "))
+operation = input("Please enter the operation you want to preform: ")
+print(calculator(num1, num2, operation))
